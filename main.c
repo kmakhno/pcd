9399b63fb834a448f22ed5c658cb9ac0938b13ae
kmakhno <kostiantynmakhno@gmail.com>
2022-12-22 22:12:05 +0200

    Pseudo Device: Add cdev member and init it
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <linux/slab.h>
 #include <linux/fs.h>
 #include <linux/kdev_t.h>
+#include <linux/cdev.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Kostiantyn Makhno");
@@ -10,11 +11,21 @@ MODULE_DESCRIPTION("Simple pseudo character driver");
 
 #define DEV_MEM_SIZE 512
 struct pcd_dev {
+	struct cdev pcd_device;
 	dev_t dev_num;
 	char dev_buff[DEV_MEM_SIZE];
 };
 
 static struct pcd_dev *dev;
+
+const struct file_operations pcd_fops = {
+	.owner = THIS_MODULE,
+	.open = pcd_open,
+	.release = pcd_release,
+	.read = pcd_read,
+	.write = pcd_write,
+	.llseek = pcd_llseek
+};
 
 static int __init pcd_init(void)
 {
@@ -28,6 +39,9 @@ static int __init pcd_init(void)
 	ret = alloc_chrdev_region(&dev->dev_num, 0, 1, "pcd");
 	if (ret < 0)
 		goto out_acr;
+
+	cdev_init(&dev->pcd_device, &pcd_fops);
+	dev->pcd_device.owner = THIS_MODULE;
 
 	pr_info("new device was allocated with major:minor %u:%u\n", 
 	        MAJOR(dev->dev_num), MINOR(dev->dev_num));
